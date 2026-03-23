9639724f949ab30ee49365c25b843ae0eb1d0f9e
slackey369 <122306090+slackey369@users.noreply.github.com>
2026-03-23 17:42:48 -0500

    Update block.py

--- src/blockchain/block.py
+++ src/blockchain/block.py
@@ -1,20 +1,20 @@
+import time
+from blocks import Transaction, InvoiceBlock
+from blockchain import Blockchain
 
-import streamlit as st
-import pandas as pd
-import numpy as np
-from hashlib import sha256
-import json
+# Initialize
+my_chain = Blockchain()
 
-#more attributes may have to be added inside class Block
-#different document types will have different attributes under "business_data", so use inheritance, on child class for each doc type
-class Block: 
-    def __init__(self, index, transactions, timestamp, document_type, previous_hash):
-        self.index = index
-        self.transactions = transactions
-        self.timestamp = timestamp
-        self.document_type = document_type
-        self.previous_hash = previous_hash
-    def compute_hash(self):
-        block_string = json.dumps(self.__dict__, sort_keys=True)
-        return sha256(block_string.encode()).hexidigest()
-    
+# Create a block
+new_invoice = InvoiceBlock(
+    index=1,
+    transactions=[],
+    timestamp=time.time(),
+    previous_hash="", 
+    creator_handle="bryan_m",
+    invoice_number="INV-100",
+    amount=500.00
+)
+
+# Add to chain
+my_chain.add_block(new_invoice)
